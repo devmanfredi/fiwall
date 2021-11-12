367afaeb04d2baa4778a50accb194243dee70e7c
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-11-11 23:00:30 -0300

    create project

---
+++ src/main/java/com/fiwall/FiwallApplication.java
@@ -0,0 +1,13 @@
+package com.fiwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiwallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiwallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiwall/FiwallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiwallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
