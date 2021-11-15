339bdce649dd233a0e8c50868e7f2dc7fd8b29dd
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-11-15 15:59:23 -0300

    develop - account repository

---
+++ src/main/java/com/fiwall/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.fiwall.repository;
+
+import com.fiwall.model.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, UUID> {
+}
