834ebe8d6bb7d05d365900b13ff615b0d86a329f
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-11-15 20:00:48 -0300

    wallet - generate number account

--- src/main/java/com/fiwall/service/AccountService.java
+++ src/main/java/com/fiwall/service/AccountService.java
@@ -1,26 +1,33 @@
 package com.fiwall.service;
 
 import com.fiwall.model.Account;
+import com.fiwall.repository.AccountRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
+@AllArgsConstructor
 public class AccountService {
 
     public static final String PIC_PAY_PAGAMENTOS = "PicPay Pagamentos";
+    private final AccountRepository accountRepository;
 
     public Account createAccount() {
         var account = new Account();
         account.setNumberAccount(generateNumber());
         account.setAgency(1);
         account.setInstitution(PIC_PAY_PAGAMENTOS);
-        account.setId(UUID.randomUUID());
+        accountRepository.save(account);
         return account;
     }
 
     private Integer generateNumber() {
-        return ThreadLocalRandom.current().nextInt();
+        var numberAccount = ThreadLocalRandom.current().nextInt();
+        if (numberAccount < 0) {
+            numberAccount = numberAccount * -1;
+        }
+        return numberAccount;
     }
 }
