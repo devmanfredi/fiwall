f5c33a56f4355de3fa59b6b77ca6ad6ec82c219f
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-11-16 15:58:44 -0300

    timeline - add id in dto for create wallet

--- src/main/java/com/fiwall/dto/UserResponseDto.java
+++ src/main/java/com/fiwall/dto/UserResponseDto.java
@@ -5,20 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
-    @Size(min = 8)
-    @NotNull
-    @Email
-    private String email;
 
-    @NotNull
+    private Long userId;
+    private String email;
     private String password;
 }
