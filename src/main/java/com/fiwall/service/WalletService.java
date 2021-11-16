2b59241afbb5e67acf4166842d845559b6b7b434
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-11-16 14:32:09 -0300

    timeline - get timeline

--- src/main/java/com/fiwall/service/WalletService.java
+++ src/main/java/com/fiwall/service/WalletService.java
@@ -1,18 +1,25 @@
 package com.fiwall.service;
 
+import com.fiwall.model.Timeline;
 import com.fiwall.model.Wallet;
+import com.fiwall.repository.TimelineRepository;
 import com.fiwall.repository.WalletRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class WalletService {
 
     private final WalletRepository walletRepository;
+    private final TimelineRepository timelineRepository;
 
     @Transactional
     public Wallet create(Wallet wallet) {
@@ -26,5 +33,11 @@ public class WalletService {
 
     public void updateBalance(Wallet wallet) {
         walletRepository.save(wallet);
+    }
+
+    public Page<Timeline> getTimeline(UUID id, Integer page, Integer size, Sort sort) {
+        var pageRequest = PageRequest.of(page, size, sort);
+
+        return timelineRepository.findAllByWalletId(id, pageRequest);
     }
 }
